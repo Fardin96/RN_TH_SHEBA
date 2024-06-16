866de12e4edfc2cf64a0bd8ed6a0efd4289ae371
Fardin96 <fardinshuvro96@gmail.com>
2024-06-16 20:18:23 +0600

    app.tsx -> app.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}>
+      <Text style={{color: 'black'}}>sheba group</Text>
+    </View>
+  );
+};
+
+export default App;
